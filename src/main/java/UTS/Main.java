5564c6902242be70fc8221e2b6e7844dcb1a7bc6
SyalomielePratama <129130206+SyalomielePratama@users.noreply.github.com>
2023-11-26 13:39:58 +0700

    Add files via upload

---
+++ src/main/java/UTS/Main.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package UTS;
+
+/**
+ *
+ * @author kunyukmini
+ */
+public class Main {
+    public static void main(String[] args) {
+        Login lg = new Login();
+        lg.setVisible(true);
+        lg.pack();
+        lg.setLocationRelativeTo(null);
+        lg.setDefaultCloseOperation(Login.EXIT_ON_CLOSE);
+    }
+    
+}
